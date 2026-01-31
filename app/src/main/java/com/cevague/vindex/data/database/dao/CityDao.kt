668afaf6aa2a9e1ad9dcdb9c68569cbe1c4ba194
CeVague <cedric.vachaudez@netc.fr>
2026-01-31 12:37:09 +0100

    Faster findNearestCity

--- app/src/main/java/com/cevague/vindex/data/database/dao/CityDao.kt
+++ app/src/main/java/com/cevague/vindex/data/database/dao/CityDao.kt
@@ -15,16 +15,38 @@ interface CityDao {
     @Query("SELECT * FROM cities WHERE name = :name AND country_code = :countryCode LIMIT 1")
     suspend fun getCityByNameAndCountry(name: String, countryCode: String): City?
 
-    @Query(
-        """
+
+    suspend fun findNearestCity(lat: Double, lon: Double): City? {
+        // Deltas progressifs : ~1km, ~10km, ~100km, ~1000km
+        val deltas = listOf(0.01, 0.1, 1.0, 10.0)
+
+        for (delta in deltas) {
+            val city = findNearestCityWithDelta(lat, lon, delta)
+            if (city != null) return city
+        }
+
+        return findNearestCitySlow(lat, lon)
+    }
+
+    @Query("""
+        SELECT *, 
+        ((:lat - latitude) * (:lat - latitude) + (:lon - longitude) * (:lon - longitude)) AS distance
+        FROM cities 
+        WHERE latitude BETWEEN :lat - :delta AND :lat + :delta
+          AND longitude BETWEEN :lon - :delta AND :lon + :delta
+        ORDER BY distance ASC 
+        LIMIT 1
+    """)
+    suspend fun findNearestCityWithDelta(lat: Double, lon: Double, delta: Double): City?
+
+    @Query("""
         SELECT *, 
         ((:lat - latitude) * (:lat - latitude) + (:lon - longitude) * (:lon - longitude)) AS distance
         FROM cities 
         ORDER BY distance ASC 
         LIMIT 1
-    """
-    )
-    suspend fun findNearestCity(lat: Double, lon: Double): City?
+    """)
+    suspend fun findNearestCitySlow(lat: Double, lon: Double): City?
 
     @Query("SELECT COUNT(*) FROM cities")
     suspend fun getCount(): Int
